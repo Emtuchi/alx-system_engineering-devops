20af47c843b470b621f57569331803cf4f91953f
George Emetu <g.emetu11@gmail.com>
2023-10-27 22:13:29 +0100

    task 1a

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
-"""using rest api"""
-import requests as r
+""" module doc """
+import requests
 import sys
-import json
 
-if __name__ == '__main__':
-    name_url = 'https://jsonplaceholder.typicode.com/users'
-    todo_url = 'https://jsonplaceholder.typicode.com/todos'
-    employee_id = sys.argv[1]
-    params = {"id" : employee_id}
-    params2 = {"userId" : employee_id}
+def main():
+    """ def com """
+    id = sys.argv[1]
+    url = f'https://jsonplaceholder.typicode.com/'
+    users = f'users?id={id}'
+    todos = f'todos?userId={id}'
+    done = f'{todos}&completed=true'
+    notDone = f'{todos}&completed=false'
+    userData = requests.get(f'{url}{users}').json()
+    Name = userData[0].get("name")
+    todosData = requests.get(f'{url}{todos}').json()
+    todosDone = requests.get(f'{url}{done}').json()
+    doneN = len(todosDone)
+    totalN = len(todosData)
+    print(f'Employee {Name} is done with tasks({doneN}/{totalN}):')
+    for task in todosDone:
+        print("\t "+task.get("title"))
 
-    user = r.get(name_url, params).json()
-    tasks = r.get(todo_url, params2).json()
 
-    name = ""
-    for user_dict in user:
-        name = user_dict.get('name')
-
-    tasks_num = 0
-    done_task = 0
-    for task in tasks:
-        tasks_num += 1
-
-        if task['completed']:
-            done_task += 1
-
-    print("Employee {} is done with tasks ({}/{}):".format
-            (name, done_task, tasks_num))
-
-    for title in tasks:
-        if title['completed']:
-            print("\t"+title['title'])
+if __name__ == "__main__":
+    main()
