3c4205bb0d40ff9d2f6e1bbe99126a5c56e64ae1
George Emetu <g.emetu11@gmail.com>
2023-10-27 21:56:37 +0100

    task 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,14 +2,33 @@
 """using rest api"""
 import requests as r
 import sys
+import json
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
-    completed = [title.get("title") for title in to_do if
-            title.get('completed') is True]
-    print(completed)
-    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
-                                                             len(completed),
-                                                             len(to_do)))
-    [print("\t {}".format(title)) for title in completed]
+    name_url = 'https://jsonplaceholder.typicode.com/users'
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    employee_id = sys.argv[1]
+    params = {"id" : employee_id}
+    params2 = {"userId" : employee_id}
+
+    user = r.get(name_url, params).json()
+    tasks = r.get(todo_url, params2).json()
+
+    name = ""
+    for user_dict in user:
+        name = user_dict.get('name')
+
+    tasks_num = 0
+    done_task = 0
+    for task in tasks:
+        tasks_num += 1
+
+        if task['completed']:
+            done_task += 1
+
+    print("Employee {} is done with tasks ({}/{}):".format
+            (name, done_task, tasks_num))
+
+    for title in tasks:
+        if title['completed']:
+            print("\t"+title['title'])
